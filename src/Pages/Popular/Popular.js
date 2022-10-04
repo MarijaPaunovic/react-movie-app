0a29866aa794f22184ef81350b2bb5f1b71444a7
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2022-10-04 12:47:02 +0200

    add data to a single content component

--- src/Pages/Popular/Popular.js
+++ src/Pages/Popular/Popular.js
@@ -8,7 +8,7 @@ function Popular() {
 
   const fetchPopular = async () => {
     const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?${apiKey}`);
-
+    // console.log(data.results);
     setContent(data.results);
   };
 
@@ -22,8 +22,17 @@ function Popular() {
       <span>Popular</span>
       <div className='popular'>
         {
-          content && content.map((c) => <SingleContent key={c.id} />)
-        }
+          content && content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.release_date || c.first_air_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+            />
+          ))}
       </div>
     </div>
   )
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { img_300, unavailableImage } from '../../config/config';
+import './SingleContent.css';
 
-function SingleContent() {
+function SingleContent({ id, poster, title, date, media_type, vote_average }) {
     return (
-        <div>SingleContent</div>
+        <div className='card'>
+            <img className='poster' src={poster ? `${img_300}/${poster}` : unavailableImage} alt={title} />
+            <div className='wrapper'>
+                <h1 className='title'>{title}</h1>
+                <p className='subTitle'>
+                    {media_type === 'tv' ? 'TV Series' : 'Movie'}
+                    <span className='subTitle'>{date}</span>
+                </p>
+            </div>
+        </div>
     )
 }
 
-export default SingleContent
+export default SingleContent;
