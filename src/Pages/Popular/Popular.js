df87e36463c00c0dbc83d74f9c3e1556326b2a77
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2022-10-04 10:43:15 +0200

    add api key and fetch init data

--- src/Pages/Popular/Popular.js
+++ src/Pages/Popular/Popular.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import SingleContent from '../../components/SingleContent/SingleContent';
+import { apiKey } from '../../config/apiKey';
 
 function Popular() {
+  const [content, setContent] = useState([]);
+
+  const fetchPopular = async () => {
+    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?${apiKey}`);
+
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchPopular();
+  }, [])
+
+
   return (
     <div>
-        <span>Popular</span>
+      <span>Popular</span>
+      <div className='popular'>
+        {
+          content && content.map((c) => <SingleContent key={c.id} />)
+        }
+      </div>
     </div>
   )
 }
---
+++ src/components/SingleContent/SingleContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingleContent() {
+    return (
+        <div>SingleContent</div>
+    )
+}
+
+export default SingleContent
